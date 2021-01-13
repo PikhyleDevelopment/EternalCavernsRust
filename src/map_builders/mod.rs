e47e299bce145a4d663f59acaeb2986b611017d2
Erik Mason <pikhyledevelopment@aol.com>
2021-01-13 02:06:44 -0500

    Cellular Automata Map Generation. Cleaned up unused use statements and dead variables. Fixed entity spawning in walls issue.

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -2,7 +2,11 @@ mod simple_map;
 use simple_map::SimpleMapBuilder;
 mod bsp_dungeon;
 mod bsp_interior;
+mod cellular_automata;
 mod common;
+
+use cellular_automata::CellularAutomataBuilder;
+
 use bsp_interior::BspInteriorBuilder;
 
 use bsp_dungeon::BspDungeonBuilder;
@@ -23,10 +27,11 @@ pub trait MapBuilder {
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     // Randomize MapBuilder per dungeon level
     let mut rng = rltk::RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 3);
+    let builder = rng.roll_dice(1, 4);
     match builder {
         1 => Box::new(BspDungeonBuilder::new(new_depth)),
         2 => Box::new(BspInteriorBuilder::new(new_depth)),
-        _ => Box::new(SimpleMapBuilder::new(new_depth))
+        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+        _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
 }
